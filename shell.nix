8bc439f881943d60ad58e10cd9bd5e720b332b6d
Miguel Negrao <miguel.negrao@friendlyvirus.org>
2015-03-21 14:01:41 +0000

    nix expression for pfVisualizer

---
+++ shell.nix
@@ -0,0 +1,18 @@
+with (import <nixpkgs> {}).pkgs;
+let pkg = haskellngPackages.callPackage
+            ({ mkDerivation, base, containers, GLUT, hosc, OpenGL, random, safe
+             , split, stdenv, stm
+             }:
+             mkDerivation {
+               pname = "pfVisualizer";
+               version = "0.1";
+               src = ./.;
+               isLibrary = false;
+               isExecutable = true;
+               buildDepends = [
+                 base containers GLUT hosc OpenGL random safe split stm
+               ];
+               license = stdenv.lib.licenses.gpl3;
+             }) {};
+in
+  pkg.env
